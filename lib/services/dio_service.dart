246fe81f4e6415bcce8cc503d5dfe22c9680d06f
kevinrisqi <kevinrisqi22@gmail.com>
2024-03-20 09:05:14 +0700

    feat: :art: setup dio service

---
+++ lib/services/dio_service.dart
@@ -0,0 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:swift_sales_pro_fe/core/constants/api_base_url.dart';
+
+class DioService {
+  DioService._();
+
+  static Dio call({
+    Duration connectTimeout = const Duration(seconds: 10),
+    String? customBaseURL,
+    String? customAuthorizationToken,
+    Map<String, dynamic>? customHeader,
+  }) {
+    String token = '';
+    Map<String, dynamic> header = {};
+
+    if (customAuthorizationToken == null || customAuthorizationToken.isEmpty) {
+      // var userData =
+      //     HiveService.get<UserData>(keyValue: HiveConstant.hiveKeyUserData);
+
+      // if (userData != null) {
+      //   /// * For testing purpose
+      //   /// * Expired token
+
+
+      //   token = userData.token ?? '';
+      // }
+    } else {
+      token = customAuthorizationToken;
+    }
+
+    header = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    header.addAll(customHeader ?? {});
+
+    var dio = Dio(
+      BaseOptions(
+        headers: header,
+        baseUrl: customBaseURL ?? AppConstant.baseUrl,
+        connectTimeout: connectTimeout,
+      ),
+    );
+
+    return dio;
+  }
+}
